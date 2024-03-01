514af85ef62e802a73bc6398cedaf2ded904c7b7
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2024-03-01 15:00:17 +0200

    Diff

--- app/page.js
+++ app/page.js
@@ -6,12 +6,16 @@ import { useState } from "react";
 export default function Home() {
   const [queueSize, setQueueSize] = useState(0);
 
+  const getSet = () => {
+    const response = getQueSize();
+    setQueueSize(response);
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-5">
       <Link href="/que" className=" px-8 py-3 bg-blue-800 text-white font-semibold rounded-md">Buy tickets</Link>
       <button onClick={() => addToQue()} className=" px-8 py-3 bg-green-500 text-white font-semibold rounded-md active:scale-95">Add to que</button>
       <button onClick={() => popFromQue()} className=" px-8 py-3 bg-red-500 text-white font-semibold rounded-md active:scale-95">Pop from que</button>
-      <button onClick={() => setQueueSize(getQueSize())} className=" px-8 py-3 bg-yellow-500 text-white font-semibold rounded-md active:scale-95">getQueSize</button>
+      <button onClick={() => getSet()} className=" px-8 py-3 bg-yellow-500 text-white font-semibold rounded-md active:scale-95">getQueSize</button>
       <h2>Queue size: {queueSize}</h2>
     </main>
   );
